91322335c392cb361574b4d089beb73937e0af51
Rafal Czarnecki <ravczar@gmail.com>
2019-10-19 09:50:12 +0200

    Adding DatabaseImpl and DatabaseTest units.

---
+++ carsApp/src/main/java/cars/s16281/tau/labone/services/DbImpl.java
@@ -0,0 +1,12 @@
+package cars.s16281.tau.labone.services;
+
+import java.util.ArrayList;
+
+public class DbImpl 
+{
+    public Integer numberOfEntries;
+    public ArrayList<CarImpl> carList = new ArrayList<CarImpl>();
+
+    public DbImpl(){}
+
+}
