04de0824795683c69cd33012d15d49bce5e3c44d
ravczar <ravczar@gmail.com>
2019-12-07 16:51:01 +0100

    Lab5: start new project and try to open new chrome browser.

---
+++ LAB5/automation/src/test/java/tau/lab5/selenium/SeleniumTest.java
@@ -0,0 +1,34 @@
+package tau.lab5.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+
+public class SeleniumTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void driverSetup(){
+        System.setProperty(
+            "webdriver.chrome.driver",
+            "C:/WebDrivers/chromedriver_win32/chromedriver.exe");  
+        WebDriver driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("Test 1 odpalono");
+        assertTrue(true);
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("Test 2 odpalono");
+        assertTrue(true);
+    }
+
+}
