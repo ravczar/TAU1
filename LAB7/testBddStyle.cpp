d1fda214d0642c78ca3d1beefc0f42e351e98829
ravczar <ravczar@gmail.com>
2020-01-15 00:28:44 +0100

    BDD style tests (add object, delete object, find object)

--- LAB7/testBddStyle.cpp
+++ LAB7/testBddStyle.cpp
@@ -1,5 +1,56 @@
 #define CATCH_CONFIG_MAIN
+#include <iostream>
 #include "lib/catch.hpp"
 #include "services/Iterator.h"
 #include "services/Car.h"
 #include "services/DataBase.h"
+
+SCENARIO( "Items can be added, deleted, found by id", "[DataBase]" ) {
+    GIVEN("Database object is filled with 3 records") {
+        DataBase db;
+        REQUIRE_NOTHROW(db.getId());
+        Car car0("DB7", "Aston Martin", 3399.00);
+        Car car1("s600", "Mercedes Benz", 4200.00);
+        Car car2("Panda", "Fiat", 1211.00 );
+        REQUIRE_NOTHROW(car0.getId());
+        db.addCar(car0);
+        db.addCar(car1);
+        db.addCar(car2);
+        REQUIRE(db.getListSize() == 3);
+        
+        WHEN( "When I add one object to database and i succeed" ) {
+            Car car4("Note", "Nissan", 1400.00);
+            db.addCar(car4);
+            THEN("Size of DB increases to '4'."){
+                REQUIRE(db.getListSize() == 4);
+                REQUIRE(db.getListSize() == db.getId());
+            }
+        }
+
+        WHEN( "I try to delete a record and I succeed" ) {
+            // DB now is again 3 items as in 'GIVEN'
+            // DB id's starts from 0, so item to be removed should be of id==2.
+            //cout<< "BAZA DANYCH MA TERAZ rekordów: "<< db.getListSize()<<endl;
+            THEN("I get a copy of it in return "){
+                REQUIRE(db.deleteCarById(2).getId() == 2);
+            }
+            AND_THEN("Size of DB decreases to '2'."){
+                db.deleteCarById(2).getId();
+                REQUIRE(db.getListSize() == 2);
+                REQUIRE_FALSE(db.getListSize() == db.getId()); // Id w klasie Iteratora będzie nadal wyższe, bo kasowanie nie zmiejsza licznika
+            }
+        }
+
+        WHEN( "I try to find item by id and I succeed" ) {
+            Car car4("Testarossa", "Ferrari", 3200.00);
+            db.addCar(car4);
+            THEN("I get a copy of object in return"){
+                //cout<<"CO JEST W TYM ITEMIE 4 brand: "<<db.getCarById(3).getBrand()<<endl;
+                REQUIRE(db.getCarById(3).getBrand() == "Ferrari");
+                REQUIRE(db.getCarById(3).getId() == 3);
+            }
+        }
+
+
+    }
+}
