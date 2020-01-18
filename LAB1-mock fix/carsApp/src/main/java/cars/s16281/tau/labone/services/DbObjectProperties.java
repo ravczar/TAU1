991ce7029bf8b7bad1073d0de5bb98c82f40ae6e
ravczar <ravczar@gmail.com>
2020-01-18 12:16:17 +0100

    Add geters and setters to DbObjectProperties in order to switch between record and do_not_record time_and_date.

--- LAB1-mock fix/carsApp/src/main/java/cars/s16281/tau/labone/services/DbObjectProperties.java
+++ LAB1-mock fix/carsApp/src/main/java/cars/s16281/tau/labone/services/DbObjectProperties.java
@@ -7,4 +7,31 @@ public class DbObjectProperties {
     private static boolean trackCreationDate = true;
     private static boolean trackModificationDate = true;
     private static boolean trackAccessDate = true;
+
+    // Setters
+    public static void setTrackCreationDate(boolean trackCreationDate) {
+        DbObjectProperties.trackCreationDate = trackCreationDate;
+    }
+
+    public static void setTrackModificationDate(boolean trackModificationDate) {
+        DbObjectProperties.trackModificationDate = trackModificationDate;
+    }
+
+    public static void setTrackAccessDate(boolean trackAccessDate) {
+        DbObjectProperties.trackAccessDate = trackAccessDate;
+    }
+
+    // Getters
+    public static boolean isTrackCreationDate() {
+        return trackCreationDate;
+    }
+
+    public static boolean isTrackModificationDate() {
+        return trackModificationDate;
+    }
+
+    public static boolean isTrackAccessDate() {
+        return trackAccessDate;
+    }
+
 }
