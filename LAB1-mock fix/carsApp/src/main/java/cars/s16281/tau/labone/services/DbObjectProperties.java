ca5de0231a06c4966c24bc74bd328825b80695fb
ravczar <ravczar@gmail.com>
2020-01-18 12:14:15 +0100

    Add class DbObjectProperties to track if we want to record R/M/C dates or not (it must be optional)

---
+++ LAB1-mock fix/carsApp/src/main/java/cars/s16281/tau/labone/services/DbObjectProperties.java
@@ -0,0 +1,10 @@
+package cars.s16281.tau.labone.services;
+
+public class DbObjectProperties {
+
+    private DbObjectProperties() {}
+
+    private static boolean trackCreationDate = true;
+    private static boolean trackModificationDate = true;
+    private static boolean trackAccessDate = true;
+}
