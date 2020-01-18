9400b472d2da2c3327d2902f437230656a4cc277
ravczar <ravczar@gmail.com>
2020-01-18 10:38:47 +0100

    ImportClasses

--- LAB1-mock fix/carsApp/src/test/java/cars/s16281/tau/labone/DateMockTest.java
+++ LAB1-mock fix/carsApp/src/test/java/cars/s16281/tau/labone/DateMockTest.java
@@ -8,6 +8,11 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import static org.junit.Assert.assertNotEquals;
 
+import cars.s16281.tau.labone.services.CarImpl;
+import cars.s16281.tau.labone.services.DbImpl;
+import cars.s16281.tau.labone.services.EngineImpl;
+import cars.s16281.tau.labone.services.GearboxImpl;
+
 public class DateMockTest {
 
 
